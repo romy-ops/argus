9ab538f103e0a70158ddacb02bae5d4e20e0626b
romy-ops <2023438624@student.uitm.edu.my>
2025-01-02 21:46:19 +0800

    Adding Google Authentication

---
+++ src/firebase-config.js
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: "AIzaSyCAQBMIVrklig9S-P-BJ8-tdJ3oQRVEp0I",
+    authDomain: "friendlychat-baywatch.firebaseapp.com",
+    projectId: "friendlychat-baywatch",
+    storageBucket: "friendlychat-baywatch.firebasestorage.app",
+    messagingSenderId: "351043171097",
+    appId: "1:351043171097:web:c33f9c20e00b68b2a68b88",
+  },
+};
