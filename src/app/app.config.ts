2506a0cac3ce73e410f974779ada18d45c851960
romy-ops <2023438624@student.uitm.edu.my>
2025-01-07 11:00:59 +0800

    com

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,14 +11,6 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideFirestore(() => getFirestore()),
-      provideAuth(() => getAuth()),
-      provideFunctions(() => getFunctions()),
-      provideStorage(() => getStorage()),
-      provideMessaging(() => getMessaging())
-    ),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-baywatch","appId":"1:351043171097:web:a231838da7df83f5a68b88","storageBucket":"friendlychat-baywatch.firebasestorage.app","apiKey":"AIzaSyCAQBMIVrklig9S-P-BJ8-tdJ3oQRVEp0I","authDomain":"friendlychat-baywatch.firebaseapp.com","messagingSenderId":"351043171097"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
